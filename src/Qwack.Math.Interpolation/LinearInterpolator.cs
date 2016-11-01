fac29ca87fe99efbf11e0af71d62124207208b9e
Drawaes <seawardtim@gmail.com>
2016-11-01 21:53:26 +0000

    Added a failing test

---
+++ src/Qwack.Math.Interpolation/LinearInterpolator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Qwack.Math.Interpolation
+{
+    public class LinearInterpolator
+    {
+        public LinearInterpolator()
+        {
+
+        }
+
+        public double Interpolate(double x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/Qwack.Math.Tests/Interpolation/LinearInterpolatorFacts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Qwack.Math.Interpolation;
+using Xunit;
+
+namespace Qwack.Math.Tests.Interpolation
+{
+    public class LinearInterpolatorFacts
+    {
+        [Fact]
+        public void CanInterpolateFact()
+        {
+            var interp = new LinearInterpolator();
+            Assert.Equal(10.0,  interp.Interpolate(100.0));
+        }
+    }
+}

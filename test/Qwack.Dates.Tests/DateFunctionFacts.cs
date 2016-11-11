7209765223fb08d43de5a1abde5a328f718c0a8d
Drawaes <seawardtim@gmail.com>
2016-11-11 01:12:57 +0000

    Started date function tests

---
+++ test/Qwack.Dates.Tests/DateFunctionFacts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Qwack.Dates.Tests
+{
+    public class DateFunctionFacts
+    {
+        private static readonly Calendar EmptyCalendar = new Calendar();
+
+        [Fact]
+        public void FirstBusinessDayOfTheMonthIgnoresTime()
+        {
+            var dt = new DateTime(2016, 10, 10, 11, 54, 30);
+            Assert.Equal(new DateTime(2016, 10, 1), dt.FirstBusinessDayOfMonth(EmptyCalendar));
+        }
+
+        [Fact]
+        public void FirstBusinessDayOfTheMonthRespectsHolidaysAndWeekends()
+        {
+            var calendar = new Calendar();
+            calendar.DaysToAlwaysExclude.Add(DayOfWeek.Saturday);
+            calendar.DaysToAlwaysExclude.Add(DayOfWeek.Sunday);
+            calendar.DaysToExclude.Add(new DateTime(2016, 07, 01));
+
+            var dt = new DateTime(2016, 07, 20);
+
+            Assert.Equal(new DateTime(2016, 07, 04), dt.FirstBusinessDayOfMonth(calendar));
+        }
+    }
+}

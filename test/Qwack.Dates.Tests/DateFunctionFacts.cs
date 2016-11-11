e828f24c883a365558573c4e1fd5eab734532463
Drawaes <seawardtim@gmail.com>
2016-11-11 01:26:13 +0000

    LastBusinessDayOfTheMonthIgnoresTime tests

--- test/Qwack.Dates.Tests/DateFunctionFacts.cs
+++ test/Qwack.Dates.Tests/DateFunctionFacts.cs
@@ -29,5 +29,24 @@ namespace Qwack.Dates.Tests
 
             Assert.Equal(new DateTime(2016, 07, 04), dt.FirstBusinessDayOfMonth(calendar));
         }
+
+        [Fact]
+        public void LastBusinessDayOfTheMonthIgnoresTime()
+        {
+            var dt = new DateTime(2016, 10, 10, 11, 54, 30);
+            Assert.Equal(new DateTime(2016, 10, 31), dt.LastBusinessDayOfMonth(EmptyCalendar));
+        }
+
+        [Fact]
+        public void LastBusinessDayOfTheMonthRespectsHolidaysAndWeekends()
+        {
+            var calendar = new Calendar();
+            calendar.DaysToAlwaysExclude.Add(DayOfWeek.Saturday);
+            calendar.DaysToAlwaysExclude.Add(DayOfWeek.Sunday);
+            calendar.DaysToExclude.Add(new DateTime(2016, 10, 31));
+
+            var dt = new DateTime(2016, 10, 10);
+            Assert.Equal(new DateTime(2016, 10, 28), dt.LastBusinessDayOfMonth(calendar));
+        }
     }
 }

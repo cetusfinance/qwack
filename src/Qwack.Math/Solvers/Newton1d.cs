7cbf90be4184913960db3ae117ab392598144125
Gav <gavbrennan@hotmail.com>
2017-01-23 20:43:11 +0200

    Added some 1d solvers

---
+++ src/Qwack.Math/Solvers/Newton1d.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Qwack.Math.Solvers
+{
+    public static class Newton1d
+    {
+        public static double MethodSolve(Func<double, double> function, double initialGuess, double errorTol, int maxItterations = 1000, double bump = 1e-6)
+        {
+            double x = initialGuess;
+            int itteration = 0;
+
+            while (itteration < maxItterations && System.Math.Abs(function(x)) > errorTol)
+            {
+                var f = function(x);
+                var fBumped = function(x + bump);
+                var dfdx = (fBumped - f) / bump;
+                x -= f / dfdx;
+            }
+
+            return x;
+        }
+    }
+}
---
+++ test/Qwack.Math.Tests/Solvers/Newton1dFacts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Qwack.Math.Interpolation;
+using Xunit;
+
+namespace Qwack.Math.Tests.Solvers
+{
+    public class Newton1dFacts
+    {
+        private double QuadraticFunction1d(double input)
+        {
+            var x = input;
+            var y = 2 * x * x + 22 * x - 17;
+            return y;
+        }
+
+        [Fact]
+        public void CanSolveTameQuadratic1dFact()
+        {
+            var output = Math.Solvers.Newton1d.MethodSolve(QuadraticFunction1d, 10, 1e-8);
+            var functionOutput = QuadraticFunction1d(output);
+            Assert.Equal(0, functionOutput, 8);
+        }
+    }
+}

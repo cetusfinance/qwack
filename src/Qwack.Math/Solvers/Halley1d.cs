ffebbec6b63712a9f7c4ed0f8d3597734432dd5e
gavbrennan <gavbrennan@hotmail.com>
2017-01-24 19:39:30 +0200

    MultiDim newton, simple abstraction (#23)
    * MultiDim newton, simple abstraction
    
    * Added some 1d solvers
    
    * Added newton with derivative
    
    * Added Halley Solver

---
+++ src/Qwack.Math/Solvers/Halley1d.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Qwack.Math.Solvers
+{
+    public static class Halley1d
+    {
+        //https://en.wikipedia.org/wiki/Halley%27s_method
+        public static double MethodSolve(Func<double, double> function, double initialGuess, double errorTol, int maxItterations = 1000, double bump = 1e-6)
+        {
+            double x = initialGuess;
+            int itteration = 0;
+
+            while (itteration < maxItterations && System.Math.Abs(function(x)) > errorTol)
+            {
+                var f = function(x);
+                var fPlus = function(x + bump);
+                var fMinus = function(x - bump);
+                var dfdxPlus = (fPlus - f) / bump;
+                var dfdxMinus = (f - fMinus) / bump;
+                var dfdx = (dfdxPlus + dfdxMinus) / 2.0;
+                var d2fdx2 = (dfdxPlus - dfdxMinus) / bump;
+                x -= 2.0 * f * dfdx / (2 * dfdx * dfdx - f * d2fdx2);
+            }
+
+            return x;
+        }
+
+        public static double MethodSolve(Func<double, double> function, Func<double, double> derivativeFunction, Func<double, double> secondDerivativeFunction, double initialGuess, double errorTol, int maxItterations = 1000)
+        {
+            double x = initialGuess;
+            int itteration = 0;
+
+            while (itteration < maxItterations && System.Math.Abs(function(x)) > errorTol)
+            {
+                var f = function(x);
+                var dfdx = derivativeFunction(x);
+                var d2fdx2 = secondDerivativeFunction(x);
+                x -= 2.0 * f * dfdx / (2 * dfdx * dfdx - f * d2fdx2);
+            }
+
+            return x;
+        }
+    }
+}
---
+++ src/Qwack.Math/Solvers/Newton1d.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Qwack.Math.Solvers
+{
+    public static class Newton1d
+    {
+        public static double MethodSolve(Func<double, double> function, double initialGuess, double errorTol, int maxItterations = 1000, double bump = 1e-6)
+        {
+            double x = initialGuess;
+            int itteration = 0;
+
+            while (itteration < maxItterations && System.Math.Abs(function(x)) > errorTol)
+            {
+                var f = function(x);
+                var fBumped = function(x + bump);
+                var dfdx = (fBumped - f) / bump;
+                x -= f / dfdx;
+            }
+
+            return x;
+        }
+
+        public static double MethodSolve(Func<double, double> function, Func<double, double> derivativeFunction, double initialGuess, double errorTol, int maxItterations = 1000)
+        {
+            double x = initialGuess;
+            int itteration = 0;
+
+            while (itteration < maxItterations && System.Math.Abs(function(x)) > errorTol)
+            {
+                var f = function(x);
+                var dfdx = derivativeFunction(x);
+                x -= f / dfdx;
+            }
+
+            return x;
+        }
+    }
+}

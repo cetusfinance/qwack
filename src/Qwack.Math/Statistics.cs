a30f31e606fe71d20401949274ff94650f548054
Drawaes <seawardtim@gmail.com>
2017-01-28 16:04:25 +0000

    Fixed spelling, spacing
    Moved options to their own dll
    cleaned up references and namespaces

--- src/Qwack.Math/Statistics.cs
+++ src/Qwack.Math/Statistics.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Qwack.Math.Utils
+namespace Qwack.Math
 {
     public class Statistics
     {
--- test/Qwack.Math.Tests/Options/BlackFacts.cs
+++ test/Qwack.Math.Tests/Options/BlackFacts.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Qwack.Math.Options;
+using Qwack.Options;
 using Xunit;
 
 namespace Qwack.Math.Tests.Options
@@ -20,25 +20,25 @@ namespace Qwack.Math.Tests.Options
             var cp = "P";
 
             //zero strike put is worthless
-            var PV = BlackFuncitons.BlackPV(f, k, rf, t, vol, cp);
+            var PV = BlackFunctions.BlackPV(f, k, rf, t, vol, cp);
             Assert.Equal(0, PV, 10);
 
             //zero strike call is worth discounted fwd
             cp = "C";
-            PV = BlackFuncitons.BlackPV(f, k, rf, t, vol, cp);
+            PV = BlackFunctions.BlackPV(f, k, rf, t, vol, cp);
             Assert.Equal(System.Math.Exp(-rf*t)*f, PV, 10);
 
             //OTM option with zero vol is worthless
             vol = 0.0;
             k = f + 1;
-            PV = BlackFuncitons.BlackPV(f, k, rf, t, vol, cp);
+            PV = BlackFunctions.BlackPV(f, k, rf, t, vol, cp);
             Assert.Equal(0, PV, 10);
 
             //put-call parity at f==k
             k = f;
             vol = 0.32;
-            var PVcall = BlackFuncitons.BlackPV(f, k, rf, t, vol, "C");
-            var PVput = BlackFuncitons.BlackPV(f, k, rf, t, vol, "P");
+            var PVcall = BlackFunctions.BlackPV(f, k, rf, t, vol, "C");
+            var PVput = BlackFunctions.BlackPV(f, k, rf, t, vol, "P");
             Assert.Equal(PVcall, PVput, 10);
 
             //forward price constant wrt total variance
@@ -47,8 +47,8 @@ namespace Qwack.Math.Tests.Options
             var t2 = t * 2.0;
             var vol2 = System.Math.Sqrt(variance / t2);
 
-            var PVnear = BlackFuncitons.BlackPV(f, k, rf, t, vol, "C");
-            var PVfar = BlackFuncitons.BlackPV(f, k, rf, t2, vol2, "C");
+            var PVnear = BlackFunctions.BlackPV(f, k, rf, t, vol, "C");
+            var PVfar = BlackFunctions.BlackPV(f, k, rf, t2, vol2, "C");
             Assert.Equal(PVnear, PVfar, 10);
         }
 
@@ -63,25 +63,23 @@ namespace Qwack.Math.Tests.Options
             var cp = "P";
 
             //vega closely matches numerical estimate
-            var PV1 = BlackFuncitons.BlackPV(f, k, rf, t, vol-0.00005, cp);
-            var PV2 = BlackFuncitons.BlackPV(f, k, rf, t, vol+0.00005, cp);
+            var PV1 = BlackFunctions.BlackPV(f, k, rf, t, vol-0.00005, cp);
+            var PV2 = BlackFunctions.BlackPV(f, k, rf, t, vol+0.00005, cp);
             var vegaEst = (PV2 - PV1) / 0.0001 * 0.01;
-            var vega = BlackFuncitons.BlackVega(f, k, rf, t, vol);
+            var vega = BlackFunctions.BlackVega(f, k, rf, t, vol);
             Assert.Equal(vegaEst, vega, 6);
 
             //all else the same, more time==more vega
-            var vegaNear = BlackFuncitons.BlackVega(f, k, rf, t, vol);
-            var vegaFar = BlackFuncitons.BlackVega(f, k, rf, t*2, vol);
+            var vegaNear = BlackFunctions.BlackVega(f, k, rf, t, vol);
+            var vegaFar = BlackFunctions.BlackVega(f, k, rf, t*2, vol);
             Assert.True(vegaFar > vegaNear);
 
             //cases for zero vega
-            vega = BlackFuncitons.BlackVega(f, 0, rf, t, vol);
+            vega = BlackFunctions.BlackVega(f, 0, rf, t, vol);
             Assert.Equal(0, vega, 8);
 
-            vega = BlackFuncitons.BlackVega(f, 1e6, rf, t, vol);
+            vega = BlackFunctions.BlackVega(f, 1e6, rf, t, vol);
             Assert.Equal(0, vega, 8);
-
         }
-
     }
 }

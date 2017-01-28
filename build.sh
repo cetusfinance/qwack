72e19f2fcf728fa2500de2e32241eda27f65f56e
Drawaes <seawardtim@gmail.com>
2017-01-28 15:04:01 +0000

    Travis again

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-dotnet restore && dotnet build **/project.json
+dotnet restore
+dotnet build **/project.json
 dotnet test test/Qwack.Dates.Tests
 dotnet test test/Qwack.Math.Tests
 dotnet test test/Qwack.Core.Tests

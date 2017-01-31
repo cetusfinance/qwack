d5e7593297dbc3bbd8725d21e7e7d574ba0da649
Gav <gavbrennan@hotmail.com>
2017-01-31 20:33:18 +0200

    First checkin, non working
    More updates, still not working
    
    Add-in works, does nothing yet
    
    Closer but still not working
    
    First checkin, non working
    
    More updates, still not working
    
    Add-in works, does nothing yet
    
    Closer but still not working
    
    First working cut of date function
    
    Add period exposed to excel
    
    Ribbon.cs

---
+++ src/Qwack.Excel/Dates/BusinessDateFunctions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+using Qwack.Dates;
+using Qwack.Excel.Services;
+namespace Qwack.Excel.Dates
+{
+    public static class BusinessDateFunctions
+    {
+        [ExcelFunction(Description = "Checks if the given date is a holiday according to the specified calendars", Category = "QDates")]
+        public static object QDates_IsHoliday(
+            [ExcelArgument(Description = "The date to check")] DateTime DateToCheck, 
+            [ExcelArgument(Description = "The calendar(s) to check against")]string Calendar)
+        {
+            return ExcelHelper.Execute(() =>
+            {
+                Calendar cal;
+
+                if (!StaticDataService.Instance.CalendarProvider.Collection.TryGetCalendar(Calendar, out cal)) 
+                    return "Calendar not found in cache";
+
+                return cal.IsHoliday(DateToCheck);
+            });
+        }
+
+        [ExcelFunction(Description = "Adds a specified period to a date, adjusting for holidays", Category = "QDates")]
+        public static object QDates_AddPeriod(
+            [ExcelArgument(Description = "Starting date")] DateTime StartDate,
+            [ExcelArgument(Description = "Period specified as a string e.g. 1w")]string Period,
+            [ExcelArgument(Description = "Roll method")]string RollMethod,
+            [ExcelArgument(Description = "Calendar(s) to check against")]string Calendar)
+        {
+            return ExcelHelper.Execute(() =>
+            {
+                Calendar cal;
+                if (!StaticDataService.Instance.CalendarProvider.Collection.TryGetCalendar(Calendar, out cal))
+                    return $"Calendar {Calendar} not found in cache";
+
+                RollType rollMethod;
+                if(!Enum.TryParse<RollType>(RollMethod,out rollMethod))
+                    return $"Unknown roll method {RollMethod}";
+
+                Frequency period = new Frequency(Period);
+
+                return StartDate.AddPeriod(rollMethod, cal, period);
+
+            });
+        }
+    }
+}
---
+++ src/Qwack.Excel/Ribbon/Ribbon.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using ExcelDna.Integration.CustomUI;
+
+
+namespace Qwack.Excel.Ribbon
+{
+    [ComVisible(true)]
+    public class MyRibbon : ExcelRibbon
+    {
+        public void SayHello(IRibbonControl control1)
+        {
+            //MessageBox.Show("Hello!");
+        }
+    }
+
+}

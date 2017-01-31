1fe9773dbdad8e96e8c8a0e1f43c14053181da63
Gav <gavbrennan@hotmail.com>
2017-01-31 20:33:18 +0200

    First checkin, non working

---
+++ src/Qwack.Excel/Dates/BusinessDateFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+using Qwack.Dates;
+
+namespace Qwack.Excel.Dates
+{
+    public static class BusinessDateFunctions
+    {
+        [ExcelFunction(Description = "Checks if the given date is a holiday according to the specified calendars", Category = "QDates")]
+        public static bool QDates_IsHoliday(
+            [ExcelArgument(Description = "The date to check")] DateTime DateToCheck, 
+            [ExcelArgument(Description = "The calendar(s) to check against")]string Calendar)
+        {
+
+            //return DateToCheck.IfHolidayRoll
+            return true;
+        }
+    }
+}

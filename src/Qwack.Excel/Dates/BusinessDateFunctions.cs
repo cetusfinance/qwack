0cf9a0452ae6b975a1ae0e0eca1c7c340a6bdefa
Gav <gavbrennan@hotmail.com>
2017-02-03 20:31:49 +0200

    Closer but still not working

--- src/Qwack.Excel/Dates/BusinessDateFunctions.cs
+++ src/Qwack.Excel/Dates/BusinessDateFunctions.cs
@@ -4,19 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using ExcelDna.Integration;
 using Qwack.Dates;
-
+using Qwack.Excel.Services;
 namespace Qwack.Excel.Dates
 {
     public static class BusinessDateFunctions
     {
         [ExcelFunction(Description = "Checks if the given date is a holiday according to the specified calendars", Category = "QDates")]
-        public static bool QDates_IsHoliday(
+        public static object QDates_IsHoliday(
             [ExcelArgument(Description = "The date to check")] DateTime DateToCheck, 
             [ExcelArgument(Description = "The calendar(s) to check against")]string Calendar)
         {
+            return ExcelHelper.Execute(() =>
+            {
+                Calendar cal;
 
-            //return DateToCheck.IfHolidayRoll
-            return true;
+                if (!StaticDataService.Instance.CalendarProvider.Collection.TryGetCalendar(Calendar, out cal)) 
+                    return "Calendar not found in cache";
+
+                return cal.IsHoliday(DateToCheck);
+            });
         }
     }
 }

f17e0d9de16eb1fb6af79e9e769634a58dcbc3a9
Gav <gavbrennan@hotmail.com>
2017-02-05 20:38:20 +0200

    Add period exposed to excel

--- src/Qwack.Excel/Dates/BusinessDateFunctions.cs
+++ src/Qwack.Excel/Dates/BusinessDateFunctions.cs
@@ -24,5 +24,29 @@ namespace Qwack.Excel.Dates
                 return cal.IsHoliday(DateToCheck);
             });
         }
+
+        [ExcelFunction(Description = "Adds a specified period to a date, adjusting for holidays", Category = "QDates")]
+        public static object QDates_AddPeriod(
+            [ExcelArgument(Description = "Starting date")] DateTime StartDate,
+            [ExcelArgument(Description = "Period specified as a string e.g. 1w")]string Period,
+            [ExcelArgument(Description = "Roll method")]string RollMethod,
+            [ExcelArgument(Description = "Calendar(s) to check against")]string Calendar)
+        {
+            return ExcelHelper.Execute(() =>
+            {
+                Calendar cal;
+                if (!StaticDataService.Instance.CalendarProvider.Collection.TryGetCalendar(Calendar, out cal))
+                    return $"Calendar {Calendar} not found in cache";
+
+                RollType rollMethod;
+                if(!Enum.TryParse<RollType>(RollMethod,out rollMethod))
+                    return $"Unknown roll method {RollMethod}";
+
+                Frequency period = new Frequency(Period);
+
+                return StartDate.AddPeriod(rollMethod, cal, period);
+
+            });
+        }
     }
 }

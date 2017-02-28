55d6891f814959f21cb479ff199cdfd3a937d16a
Drawaes <seawardtim@gmail.com>
2017-02-28 04:45:23 +0000

    Fix coverage

--- script/runtests.bat
+++ script/runtests.bat
@@ -1,3 +1,4 @@
 dotnet test test/Qwack.Dates.Tests/Qwack.Dates.Tests.csproj
 dotnet test test/Qwack.Math.Tests/Qwack.Math.Tests.csproj
 dotnet test test/Qwack.Core.Tests/Qwack.Core.Tests.csproj
+dotnet test test/Qwack.MonteCarlo.Test/Qwack.MonteCarlo.Test.csproj

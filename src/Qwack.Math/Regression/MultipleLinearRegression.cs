e10b2e6208fad759f8508230f335e0a6d8022a31
Gav <gavbrennan@hotmail.com>
2017-03-02 21:33:21 +0200

    MLR, working

---
+++ src/Qwack.Math/Regression/MultipleLinearRegression.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Qwack.Math.Regression
+{
+    public class MultipleLinearRegression
+    {
+        //http://dept.stat.lsa.umich.edu/~kshedden/Courses/Stat401/Notes/401-multreg.pdf
+        //examples in rows, weights in columns
+        public static double[] Regress(double[][] predictors, double[] predictions)
+        {
+            if (predictors.Length != predictions.Length)
+                throw new Exception("Number of predictor rows should equal the number of predictions");
+    
+            double[] weights = new double[predictors[0].Length + 1]; //first element is intercept
+
+            double[][] designMatrix = Matrix.DoubleArrayFunctions.MatrixCreate(predictors.Length, predictors[0].Length + 1);
+
+
+            for(int r=0;r<predictions.Length;r++)
+            {
+                designMatrix[r][0] = 1.0;
+                for(int c=0;c<predictors[0].Length;c++)
+                {
+                    designMatrix[r][c + 1] = predictors[r][c];
+                }
+            }
+
+            //do the math
+            var X = designMatrix;
+            var Xt = Matrix.DoubleArrayFunctions.Transpose(X);
+            var Z1 = Matrix.DoubleArrayFunctions.MatrixProduct(Xt, X);
+            var Z2 = Matrix.DoubleArrayFunctions.InvertMatrix(Z1);
+            var Z3 = Matrix.DoubleArrayFunctions.MatrixProduct(Z2, Xt);
+            weights = Matrix.DoubleArrayFunctions.MatrixProduct(Z3, predictions);
+            return weights;
+        }
+    }
+}
---
+++ test/Qwack.Math.Tests/Regression/MultipleLinearRegressionFacts.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Qwack.Math.Tests.Regression
+{
+    public class MultipleLinearRegressionFacts
+    {
+        [Fact]
+        public void dim2Facts()
+        {
+            double intercept = 76;
+            double w1 = 5;
+            double w2 = -2;
+
+            Func<double[], double> testFunc = new Func<double[], double>(xs => 
+            {
+                return intercept + xs[0] * w1 + xs[1] * w2;
+            });
+
+            int nExamples = 100;
+            double[][] predictors = new double[nExamples][];
+            double[] predictions = new double[nExamples];
+
+            var R = new System.Random();
+
+            for (int e=0;e<nExamples;e++)
+            {
+                predictors[e] = new double[2] { R.NextDouble(), R.NextDouble() };
+                predictions[e] = testFunc(predictors[e]);
+            }
+
+            var weights = Math.Regression.MultipleLinearRegression.Regress(predictors, predictions);
+            Assert.Equal(intercept, weights[0],8);
+            Assert.Equal(w1, weights[1],8);
+            Assert.Equal(w2, weights[2],8);
+
+        }
+
+        [Fact]
+        public void dim2NRFacts()
+        {
+            double[] ws0 = new double[] { 76, 5, -2 };
+            
+            int nExamples = 100;
+            double[][] predictors = new double[nExamples][];
+            double[] predictions = new double[nExamples];
+
+            var R = new System.Random();
+            for (int e = 0; e < nExamples; e++)
+            {
+                predictors[e] = new double[2] {  R.NextDouble(), R.NextDouble() };
+            }
+
+            Func<double[], double[], double> testFunc = new Func<double[], double[], double>((xs,ws) =>
+            {
+                var intercept = ws[0];
+                var w1 = ws[1];
+                var w2 = ws[2];
+                return intercept + xs[0] * w1 + xs[1] * w2;
+            });
+
+            Func<double[], double[]> solveFunc = new Func<double[], double[]>(ws =>
+            {
+                return predictors.Select(x => testFunc(x,ws)-testFunc(x,ws0)).ToArray();
+            });
+
+            var solver = new Math.Solvers.GaussNewton
+            {
+                ObjectiveFunction = solveFunc,
+                InitialGuess = new double[3]
+            };
+
+            var weights = solver.Solve();
+            Assert.Equal(ws0[0], weights[0],8);
+            Assert.Equal(ws0[1], weights[1],8);
+            Assert.Equal(ws0[2], weights[2],8);
+
+        }
+    }
+}

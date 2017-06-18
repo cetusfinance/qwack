55a7394c2d927f8948db6d08787c5bb2784f3c66
Drawaes <seawardtim@gmail.com>
2017-06-18 03:15:20 +0100

    AsianPut first cut

--- src/Qwack.Paths/Payoffs/AsianPut.cs
+++ src/Qwack.Paths/Payoffs/AsianPut.cs
@@ -6,20 +6,20 @@ using Qwack.Paths.Features;
 
 namespace Qwack.Paths.Payoffs
 {
-    public class Put : IPathProcess, IFeatureRequiresFinish
+    public class AsianPut : IPathProcess, IFeatureRequiresFinish
     {
-        private DateTime _expiry;
+        private List<DateTime> _asianDates;
         private double _strike;
         private string _assetName;
         private int _assetIndex;
-        private int _expiryIndex;
+        private int[] _dateIndexes;
         private List<Vector<double>> _results = new List<Vector<double>>();
 
-        public Put(string assetName, double strike, DateTime expiry)
+        public AsianPut(string assetName, double strike, List<DateTime> asianingDates)
         {
-            _expiry = expiry;
-            _strike = strike;
+            _asianDates = asianingDates;
             _assetName = assetName;
+            _strike = strike;
         }
 
         public void Finish(FeatureCollection collection)
@@ -28,15 +28,24 @@ namespace Qwack.Paths.Payoffs
             _assetIndex = dims.GetDimension(_assetName);
 
             var dates = collection.GetFeature<ITimeStepsFeature>();
-            _expiryIndex = dates.GetDateIndex(_expiry);          
+            _dateIndexes = new int[_asianDates.Count];
+            for(var i = 0; i < _asianDates.Count; i++)
+            {
+                _dateIndexes[i] = dates.GetDateIndex(_asianDates[i]);
+            }
         }
 
         public void Process(PathBlock block)
         {
-            for(var path = 0; path < block.NumberOfPaths;path += Vector<double>.Count)
+            for (var path = 0; path < block.NumberOfPaths; path += Vector<double>.Count)
             {
                 var steps = block.GetStepsForFactor(path, _assetIndex);
-                var finalValues = (new Vector<double>(_strike)) - steps[_expiryIndex];
+                var finalValues = new Vector<double>(0.0);
+                for(var i = 0; i< _dateIndexes.Length;i++)
+                {
+                    finalValues += steps[_dateIndexes[i]];
+                }
+                finalValues = (new Vector<double>(_strike)) - (finalValues /  new Vector<double>(_dateIndexes.Length));
                 finalValues = Vector.Max(new Vector<double>(0), finalValues);
                 _results.Add(finalValues);
             }
@@ -45,7 +54,7 @@ namespace Qwack.Paths.Payoffs
         public void SetupFeatures(FeatureCollection pathProcessFeaturesCollection)
         {
             var dates = pathProcessFeaturesCollection.GetFeature<ITimeStepsFeature>();
-            dates.AddDate(_expiry);
+            dates.AddDates(_asianDates);
         }
     }
 }

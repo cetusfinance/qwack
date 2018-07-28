37347186115678934d1a34e8aeca3fef3792f69c
Tim Seaward <seawardtim@gmail.com>
2018-07-28 01:25:38 +0100

    Fixing code coverage reports

--- script/runtests.bat
+++ script/runtests.bat
@@ -2,4 +2,3 @@ dotnet test test/Qwack.Dates.Tests/Qwack.Dates.Tests.csproj
 dotnet test test/Qwack.Math.Tests/Qwack.Math.Tests.csproj
 dotnet test test/Qwack.Core.Tests/Qwack.Core.Tests.csproj
 dotnet test test/Qwack.Excel.Tests/Qwack.Excel.Tests.csproj
-dotnet test test/Qwack.MonteCarlo.Test/Qwack.MonteCarlo.Test.csproj

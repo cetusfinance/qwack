b7b911c2ca4ff90c408f2f2643fe24f4fccf48ee
Tim Seaward <seawardtim@gmail.com>
2018-09-06 01:14:49 +0100

    Files added still need tests for currencies

--- src/Qwack.Providers/Json/CurrenciesFromJson.cs
+++ src/Qwack.Providers/Json/CurrenciesFromJson.cs
@@ -10,23 +10,23 @@ namespace Qwack.Providers.Json
 {
     public class CurrenciesFromJson : ICurrencyProvider
     {
-        private static readonly JsonSerializerSettings _jsonSettings;
-        private Dictionary<string, Currency> _loadedCurrencies;
+        private JsonSerializerSettings _jsonSettings;
+        private ICalendarProvider _calendarProvider;
+        private List<Currency> _allCurrencies;
+        private Dictionary<string, Currency> _currenciesByName;
 
-        static CurrenciesFromJson() => _jsonSettings = new JsonSerializerSettings()
+        public CurrenciesFromJson(ICalendarProvider calendarProvider, string fileName)
         {
-            DateFormatString = "yyyyMMdd"
-        };
-
-        public static CurrenciesFromJson Parse(string jsonString, ICalendarProvider calendarProvider)
-        {
-            var returnValue = new CurrenciesFromJson()
+            _calendarProvider = calendarProvider;
+            _jsonSettings = new JsonSerializerSettings()
             {
-                _loadedCurrencies = JsonConvert.DeserializeObject<Dictionary<string, Currency>>(jsonString, _jsonSettings)
+                Converters = new JsonConverter[]
+                {
+                    new CurrencyConverter(_calendarProvider),
+                },
             };
-            return returnValue;
+            _allCurrencies = JsonConvert.DeserializeObject<List<Currency>>(System.IO.File.ReadAllText(fileName), _jsonSettings);
+            _currenciesByName = _allCurrencies.ToDictionary(c => c.Ccy, c => c, StringComparer.OrdinalIgnoreCase);
         }
-
-        public static CurrenciesFromJson Load(string filename, ICalendarProvider calendarProvider) => Parse(System.IO.File.ReadAllText(filename), calendarProvider);
     }
 }

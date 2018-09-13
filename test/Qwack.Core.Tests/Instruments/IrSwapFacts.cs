ce26d4d78fc465062495f0d127150034b3824df8
gavbrennan <gavbrennan@hotmail.com>
2018-09-13 14:10:38 +0200

    Add files via upload

---
+++ test/Qwack.Core.Tests/Instruments/IrSwapFacts.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Qwack.Core.Instruments.Funding;
+using Qwack.Core.Curves;
+using System.Linq;
+using Qwack.Math.Interpolation;
+using Qwack.Core.Basic;
+using Qwack.Models;
+using Qwack.Dates;
+using static System.Math;
+using Microsoft.Extensions.PlatformAbstractions;
+using Qwack.Providers.Json;
+
+namespace Qwack.Core.Tests.Instruments
+{
+    public class IrSwapFacts
+    {
+        public static readonly string JsonCalendarPath = System.IO.Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "Calendars.json");
+        public static readonly ICalendarProvider CalendarProvider = CalendarsFromJson.Load(JsonCalendarPath);
+
+        [Fact]
+        public void IrSwap()
+        {
+            var bd = DateTime.Today;
+            var pillars = new[] { bd, bd.AddDays(1000) };
+            var flatRate = 0.05;
+            var rates = pillars.Select(p => flatRate).ToArray();
+            var usd = new Currency("USD", Dates.DayCountBasis.Act365F, null);
+            var discoCurve = new IrCurve(pillars, rates, bd, "USD.BLAH", Interpolator1DType.Linear, usd);
+            var fModel = new FundingModel(bd, new[] { discoCurve });
+            CalendarProvider.Collection.TryGetCalendar("LON", out var cal);
+
+            var ix = new FloatRateIndex
+            {
+                Currency = usd,
+                DayCountBasis = DayCountBasis.ACT360,
+                DayCountBasisFixed = DayCountBasis.ACT360,
+                FixingOffset = 2.Bd(),
+                HolidayCalendars = cal,
+                ResetTenor = 3.Months(),
+                ResetTenorFixed = 3.Months(),
+                RollConvention = RollType.MF
+            };
+
+
+            var parRate = 0.05;
+            var notional = 100e6;
+            var startDate = bd.AddPeriod(RollType.F, cal, 2.Bd());
+            var maturity = startDate.AddDays(365);
+            var swp = new IrSwap(startDate, 1.Years(), ix, parRate, SwapPayReceiveType.Pay, "USD.BLAH", "USD.BLAH") { Notional = notional };
+
+            var pv = swp.Pv(fModel, true);
+            Assert.Equal(-368.89651349, pv, 8);
+
+            swp = new IrSwap(startDate, 1.Years(), ix, parRate+0.01, SwapPayReceiveType.Pay, "USD.BLAH", "USD.BLAH") { Notional = notional };
+            pv = swp.Pv(fModel, true);
+            Assert.Equal(-10217.8229952, pv, 8);
+
+        }
+
+    }
+}
---
+++ test/Qwack.Core.Tests/Instruments/LoanDepoFacts.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Qwack.Core.Instruments.Funding;
+using Qwack.Core.Curves;
+using System.Linq;
+using Qwack.Math.Interpolation;
+using Qwack.Core.Basic;
+using Qwack.Models;
+using Qwack.Dates;
+using static System.Math;
+
+namespace Qwack.Core.Tests.Instruments
+{
+    public class LoanDepoFacts
+    {
+        [Fact]
+        public void FixedRateLoanDepo()
+        {
+            var bd = DateTime.Today;
+            var pillars = new[] { bd, bd.AddDays(1000) };
+            var flatRate = 0.05;
+            var rates = pillars.Select(p => flatRate).ToArray();
+            var usd = new Currency("USD", Dates.DayCountBasis.Act365F, null);
+            var discoCurve = new IrCurve(pillars, rates, bd, "USD.BLAH", Interpolator1DType.Linear, usd);
+            var fModel = new FundingModel(bd, new[] { discoCurve });
+
+            var maturity = bd.AddDays(365);
+            var notional = 100e6;
+            var iRate = 0.07;
+            var depo = new FixedRateLoanDeposit(bd, maturity, iRate, usd, DayCountBasis.ACT360, notional, "USD.BLAH");
+            
+            var pv = depo.Pv(fModel, false);
+
+            var dfEnd = discoCurve.GetDf(bd, maturity);
+            var t = bd.CalculateYearFraction(maturity, DayCountBasis.ACT360);
+            var expectedPv = notional; //initial notional
+            expectedPv += -notional * dfEnd; //final notional
+            expectedPv += -notional * ( iRate * t) * dfEnd; //final notional
+            Assert.Equal(expectedPv, pv, 8);
+
+
+            var loan = new FixedRateLoanDeposit(bd, maturity, iRate, usd, DayCountBasis.ACT360, -notional, "USD.BLAH");
+            pv = loan.Pv(fModel, false);
+
+            expectedPv = -notional; //initial notional
+            expectedPv += notional * dfEnd; //final notional
+            expectedPv += notional * (iRate * t) * dfEnd; //final notional
+            Assert.Equal(expectedPv, pv, 8);
+        }
+
+
+        [Fact]
+        public void PastStartingFixedRateLoanDepo()
+        {
+            var bd = DateTime.Today;
+            var pillars = new[] { bd, bd.AddDays(1000) };
+            var flatRate = 0.05;
+            var rates = pillars.Select(p => flatRate).ToArray();
+            var usd = new Currency("USD", Dates.DayCountBasis.Act365F, null);
+            var discoCurve = new IrCurve(pillars, rates, bd, "USD.BLAH", Interpolator1DType.Linear, usd);
+            var fModel = new FundingModel(bd, new[] { discoCurve });
+
+            var start = bd.AddDays(-10);
+            var maturity = bd.AddDays(365);
+            var notional = 100e6;
+            var iRate = 0.07;
+
+            var depo = new FixedRateLoanDeposit(start, maturity, iRate, usd, DayCountBasis.ACT360, notional, "USD.BLAH");
+
+            var pv = depo.Pv(fModel, false);
+
+            var dfEnd = discoCurve.GetDf(bd, maturity);
+            var t = start.CalculateYearFraction(maturity, DayCountBasis.ACT360);
+            var expectedPv = 0.0; //initial notional is in the past
+            expectedPv += -notional * dfEnd; //final notional
+            expectedPv += -notional * (iRate * t) * dfEnd; //final notional
+            Assert.Equal(expectedPv, pv, 8);
+
+
+            var loan = new FixedRateLoanDeposit(start, maturity, iRate, usd, DayCountBasis.ACT360, -notional, "USD.BLAH");
+            pv = loan.Pv(fModel, false);
+
+            expectedPv = 0.0; //initial notional is in the past
+            expectedPv += notional * dfEnd; //final notional
+            expectedPv += notional * (iRate * t) * dfEnd; //final notional
+            Assert.Equal(expectedPv, pv, 8);
+        }
+    }
+}

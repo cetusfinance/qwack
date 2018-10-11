35de9c71784ade1e079db80653444c24beebb6a7
Tim Seaward <seawardtim@gmail.com>
2018-10-11 02:29:06 +0100

    serialisation basic working collections to come

--- src/Qwack.Serialization/BinarySerilizer.cs
+++ src/Qwack.Serialization/BinarySerilizer.cs
@@ -13,19 +13,21 @@ namespace Qwack.Serialization
         private Dictionary<Type, int> _typeNumbers = new Dictionary<Type, int>();
         private int _number = 1;
 
+        private const byte ArrayType_ULong = 1;
+
         public void PrepareObjectGraph(object objectGraph)
         {
             if (objectGraph == null) return;
             if (!objectGraph.GetType().Namespace.StartsWith("Qwack")) return;
 
             var privateFields = objectGraph.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
-            foreach(var f in privateFields)
+            foreach (var f in privateFields)
             {
                 if (f.FieldType.IsEnum) continue;
                 var value = f.GetValue(objectGraph);
                 AddField(value);
             }
-            if(!_typeNumbers.ContainsKey(objectGraph.GetType()))
+            if (!_typeNumbers.ContainsKey(objectGraph.GetType()))
             {
                 _typeNumbers.Add(objectGraph.GetType(), _number);
                 _number++;
@@ -36,6 +38,7 @@ namespace Qwack.Serialization
         public void SerializeObjectGraph(PipeWriter pipeWriter)
         {
             var span = pipeWriter.GetSpan();
+            var spanLength = span.Length;
             // Serialize all objects
             foreach (var o in _objectsCrawled)
             {
@@ -44,31 +47,121 @@ namespace Qwack.Serialization
                 span.Write((ushort)number);
                 //Now save all of the fields
                 var privateFields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
-                foreach(var pField in privateFields)
+                foreach (var pField in privateFields)
                 {
+                    pipeWriter.Advance(spanLength - span.Length);
+                    span = pipeWriter.GetSpan(2048);
+                    spanLength = span.Length;
                     if (pField.FieldType.IsEnum)
                     {
                         //Need to write the enum straight to the stream
-                        var enumType = pField.FieldType.GetEnumUnderlyingType();
-                        if(enumType == typeof(int))
+                        var enumType = pField.GetValue(o);
+                        switch (enumType)
                         {
-                            span.Write((int)pField.GetValue(o));
+                            case byte b:
+                                span.Write(b);
+                                break;
+                            case short s:
+                                span.Write(s);
+                                break;
+                            case int i:
+                                span.Write(i);
+                                break;
+                            default:
+                                throw new NotSupportedException();
+                        }
+                    }
+                    else if (pField.FieldType.Namespace.StartsWith("Qwack"))
+                    {
+                        //One of our objects so we need to look up the object number in the list and replace with that
+                        var qo = pField.GetValue(o);
+                        if (qo == null)
+                        {
+                            span.Write((ushort)0);
                         }
                         else
                         {
                             throw new NotImplementedException();
                         }
                     }
-                    else if (pField.FieldType.Namespace.StartsWith("Qwack"))
-                    {
-                        //One of our objects so we need to look up the object number in the list and replace with that
-                        throw new NotImplementedException();
-                    }
                     else
                     {
-                        //Look up our common types
-                        //If we don't find it there then throw
-                        throw new NotImplementedException();
+                        if(pField.FieldType.IsGenericType)
+                        {
+                            //Is probably a collection
+                            throw new NotImplementedException();
+                        }
+                        else if(pField.FieldType.IsArray)
+                        {
+                            //Deal with arrays
+                            var array = pField.GetValue(o);
+                            switch(array)
+                            {
+                                case ulong[] ul:
+                                    span.Write(ArrayType_ULong);
+                                    span.Write(ul.Length);
+                                    for(var i = 0; i < ul.Length;i++)
+                                    {
+                                        if(span.Length < sizeof(ulong))
+                                        {
+                                            pipeWriter.Advance(spanLength - span.Length);
+                                            span = pipeWriter.GetSpan(512);
+                                            spanLength = span.Length;
+                                        }
+                                        span.Write(ul[i]);
+                                    }
+                                    break;
+                                default:
+                                    throw new NotImplementedException();
+                            }
+                        }
+                        else
+                        {
+                            //Look up our common types
+                            //If we don't find it there then throw
+                            var simpleType = pField.GetValue(o);
+                            switch(simpleType)
+                            {
+                                case uint ui:
+                                    span.Write(ui);
+                                    break;
+                                case ulong ui64:
+                                    span.Write(ui64);
+                                    break;
+                                case long l:
+                                    span.Write(l);
+                                    break;
+                                case int i:
+                                    span.Write(i);
+                                    break;
+                                case bool b:
+                                    span.Write((byte)(b ? 1 : 0));
+                                    break;
+                                case DateTime d:
+                                    span.Write(d.Ticks);
+                                    break;
+                                case float f:
+                                    span.Write(f);
+                                    break;
+                                case double dd:
+                                    span.Write(dd);
+                                    break;
+                                case string s:
+                                    throw new NotImplementedException();
+                                case null:
+                                    if(pField.FieldType == typeof(string))
+                                    {
+                                        span.Write((uint)0);
+                                    }
+                                    else
+                                    {
+                                        throw new NotImplementedException();
+                                    }
+                                    break;
+                                default:
+                                    throw new NotImplementedException();
+                            }
+                        }
                     }
                 }
             }
--- src/Qwack.Serialization/SpanExtensions.cs
+++ src/Qwack.Serialization/SpanExtensions.cs
@@ -8,6 +8,41 @@ namespace Qwack.Serialization
 {
     internal static class SpanExtensions
     {
+        public static void Write(ref this Span<byte> span, byte value)
+        {
+            if (span.Length < sizeof(byte)) throw new IndexOutOfRangeException();
+            Unsafe.WriteUnaligned(ref MemoryMarshal.GetReference(span), value);
+            span = span.Slice(sizeof(byte));
+        }
+
+        public static void Write(ref this Span<byte> span, long value)
+        {
+            if (span.Length < sizeof(long)) throw new IndexOutOfRangeException();
+            Unsafe.WriteUnaligned(ref MemoryMarshal.GetReference(span), value);
+            span = span.Slice(sizeof(long));
+        }
+
+        public static void Write(ref this Span<byte> span, double value)
+        {
+            if (span.Length < sizeof(double)) throw new IndexOutOfRangeException();
+            Unsafe.WriteUnaligned(ref MemoryMarshal.GetReference(span), value);
+            span = span.Slice(sizeof(double));
+        }
+
+        public static void Write(ref this Span<byte> span, float value)
+        {
+            if (span.Length < sizeof(float)) throw new IndexOutOfRangeException();
+            Unsafe.WriteUnaligned(ref MemoryMarshal.GetReference(span), value);
+            span = span.Slice(sizeof(float));
+        }
+
+        public static void Write(ref this Span<byte> span, ulong value)
+        {
+            if (span.Length < sizeof(ulong)) throw new IndexOutOfRangeException();
+            Unsafe.WriteUnaligned(ref MemoryMarshal.GetReference(span), value);
+            span = span.Slice(sizeof(ulong));
+        }
+
         public static void Write(ref this Span<byte> span, ushort value)
         {
             if (span.Length < sizeof(ushort)) throw new IndexOutOfRangeException();
@@ -20,6 +55,20 @@ namespace Qwack.Serialization
             if (span.Length < sizeof(int)) throw new IndexOutOfRangeException();
             Unsafe.WriteUnaligned(ref MemoryMarshal.GetReference(span), value);
             span = span.Slice(sizeof(int));
+        }
+
+        public static void Write(ref this Span<byte> span, short value)
+        {
+            if (span.Length < sizeof(short)) throw new IndexOutOfRangeException();
+            Unsafe.WriteUnaligned(ref MemoryMarshal.GetReference(span), value);
+            span = span.Slice(sizeof(short));
+        }
+
+        public static void Write(ref this Span<byte> span, uint value)
+        {
+            if (span.Length < sizeof(uint)) throw new IndexOutOfRangeException();
+            Unsafe.WriteUnaligned(ref MemoryMarshal.GetReference(span), value);
+            span = span.Slice(sizeof(uint));
         }
     }
 }

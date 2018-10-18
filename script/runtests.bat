4e5337c892a1471dcd0ece51d9047483f710026e
Tim Seaward <seawardtim@gmail.com>
2018-10-18 01:54:01 +0100

    add tests to build

--- script/runtests.bat
+++ script/runtests.bat
@@ -2,3 +2,4 @@ dotnet test test/Qwack.Dates.Tests/Qwack.Dates.Tests.csproj
 dotnet test test/Qwack.Math.Tests/Qwack.Math.Tests.csproj
 dotnet test test/Qwack.Core.Tests/Qwack.Core.Tests.csproj
 dotnet test test/Qwack.Excel.Tests/Qwack.Excel.Tests.csproj
+dotnet test test/Qwack.Serialization.Test/Qwack.Serialization.Test.csproj

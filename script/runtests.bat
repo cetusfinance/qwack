1183360b7c971686484fea6d09d9b9bf3f0413d7
gavbrennan <gavbrennan@hotmail.com>
2019-04-23 12:32:00 +0200

    Fix for test coverage

--- script/runtests.bat
+++ script/runtests.bat
@@ -3,3 +3,5 @@ dotnet test test/Qwack.Math.Tests/Qwack.Math.Tests.csproj
 dotnet test test/Qwack.Core.Tests/Qwack.Core.Tests.csproj
 dotnet test test/Qwack.Excel.Tests/Qwack.Excel.Tests.csproj
 dotnet test test/Qwack.Serialization.Test/Qwack.Serialization.Test.csproj
+dotnet test test/Qwack.Models.Tests/Qwack.Models.Tests.csproj
+dotnet test test/Qwack.MonteCarlo.Test/Qwack.MonteCarlo.Test.csproj

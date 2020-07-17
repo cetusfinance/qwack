b25581d8f6d26a65090e22618893369c397a48d5
Gavin Brennan <gavbrennan@hotmail.com>
2020-07-17 15:38:20 +0200

    CVA capital tweaks

--- src/Qwack.Models/Risk/BaselHelper.cs
+++ src/Qwack.Models/Risk/BaselHelper.cs
@@ -15,5 +15,15 @@ namespace Qwack.Models.Risk
             LGD * (NormSDist(NormInv(PD)/Sqrt(1-R(PD))+Sqrt(R(PD)/(1-R(PD)))*NormInv(0.999))-PD) * (1 + (M - 2.5) * b(PD)) / (1 - 1.5 * b(PD));
         public static double RWA(double PD, double LGD, double M, double EAD) =>
             EAD * 12.5 * K(PD, LGD, M);
+
+        public static class BasicCvaB3
+        {
+            public const double Beta = 0.5;
+            public const double Rho = 0.5;
+            public static double kSpreadUnhedgedSingle(double riskWeight, double effMaturity, double ead) => Sc(riskWeight, effMaturity, ead);
+            public static double Sc(double riskWeight, double effMaturity, double ead) => riskWeight / 1.4 * effMaturity * ead;
+            public static double kEE(double riskWeight, double effMaturity, double ead) => Beta * kSpreadUnhedgedSingle(riskWeight, effMaturity, ead);
+            public static double cvaCapitalCharge(double riskWeight, double effMaturity, double ead) => (1 + Beta) * kSpreadUnhedgedSingle(riskWeight, effMaturity, ead);
+        }
     }
 }

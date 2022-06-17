bce97990fb2fb9994839f7f550bc950bf4c29643
Gavin Brennan <gavbrennan@hotmail.com>
2022-06-17 19:01:29 +0200

    More bond utils functions

--- src/Qwack.Math/BondUtils.cs
+++ src/Qwack.Math/BondUtils.cs
@@ -15,6 +15,10 @@ namespace Qwack.Math
                 YieldToMaturity(couponRate, callPrice, cleanPrice, tCall)
                );
 
+        public static double PriceFromYtm(double couponRate, double faceValue, double ytm, double t)
+            => (couponRate * 2 + faceValue * (2 / t - ytm)) / (ytm + 2 / t);
+
+
         public static double PriceFromYTC(double couponRate, double callPrice, double tCall, double ytc) =>
             couponRate / 2 * ((1 - System.Math.Pow(1 + ytc / 2, -2 * tCall)) / (ytc / 2)) + callPrice / System.Math.Pow( (1 + ytc / 2), 2 * tCall);
 
@@ -26,6 +30,26 @@ namespace Qwack.Math
             });
 
             return Solvers.Brent.BrentsMethodSolve(solverFn, 1e-6, 1, 1e-6);
+        }
+
+        public static double MacaulayDuration (double couponRate, double faceValue, double ytm, double periodsPerYear, double tMaturity, double tNext, double cleanPrice)
+        {
+            var nPeriods = (tMaturity-tNext) * periodsPerYear;
+            var couponFlow = couponRate * faceValue / periodsPerYear;
+            var divisor = 1 + ytm / periodsPerYear;
+            var sum = 0.0;
+
+            for(var i = 0; i <= nPeriods; i++)
+            {
+                var df = System.Math.Pow(divisor, i + 1);
+                var rowFlow = couponFlow / df * (tNext + i / periodsPerYear) / cleanPrice;
+                if (i == nPeriods)
+                {
+                    rowFlow += faceValue / df * (tNext + i / periodsPerYear) / cleanPrice;
+                }
+                sum += rowFlow;
+            }
+            return sum;
         }
     }
 }
--- test/Qwack.Math.Tests/BondUtilsFacts.cs
+++ test/Qwack.Math.Tests/BondUtilsFacts.cs
@@ -12,7 +12,7 @@ namespace Qwack.Math.Tests
     {
         [Fact]
         //https://www.investopedia.com/terms/y/yieldtocall.asp
-        public void BondUtilFacts()
+        public void YtcFromPriceFacts()
         {
             var cleanPrice = 1175.0;
             var couponRate = 0.1;
@@ -24,6 +24,37 @@ namespace Qwack.Math.Tests
             //var ytc = BondUtils.YtcFromPrice(couponRate * faceValue, callPrice , tCall, cleanPrice );
 
             Assert.Equal(0.0743, ytc, 4);
+        }
+
+        [Fact]
+        public void YtmRoundTrip()
+        {
+            var cleanPrice = 1175.0;
+            var couponRate = 0.1;
+            var faceValue = 1000.0;
+            var t = 5.0;
+
+            var ytm = BondUtils.YieldToMaturity(couponRate, faceValue, cleanPrice, t);
+            var price = BondUtils.PriceFromYtm(couponRate, faceValue, ytm, t);
+
+            Assert.Equal(cleanPrice, price, 8);
+        }
+
+        [Fact]
+        //https://www.investopedia.com/terms/d/duration.asp
+        public void MacaulayTests()
+        {
+            var cleanPrice = 110.83;
+            var couponRate = 0.1;
+            var couponsPerYear = 2.0;
+            var faceValue = 100;
+            var ytm = 0.06;
+            var t = 3.0;
+            var tNext = 0.5;
+
+            var mac = BondUtils.MacaulayDuration(couponRate, faceValue, ytm, couponsPerYear, t, tNext, cleanPrice);
+            
+            Assert.Equal(2.684, mac, 3);
         }
     }
 }

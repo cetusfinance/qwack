4da0d10536927024d906a57c6f11a83465cea58a
Gavin Brennan <gavbrennan@hotmail.com>
2022-06-20 16:39:06 +0200

    CouponFrequency.cs

---
+++ src/Qwack.Transport/BasicTypes/CouponFrequency.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Qwack.Transport.BasicTypes
+{
+    public enum CouponFrequency
+    {
+        SemiAnnual = 0,
+        SA = 0,
+        Annual = 1,
+        A = 1,
+        Quarterly = 2,
+        Q = 2,
+    }
+}

ce78d26699ed8ca35740bb17a77fcc77f1a54ded
Gavin Brennan <gavbrennan@hotmail.com>
2024-06-05 21:17:39 +0100

    Fixing fix

--- src/Qwack.Models/MCModels/AssetFxMCModel.cs
+++ src/Qwack.Models/MCModels/AssetFxMCModel.cs
@@ -167,9 +167,9 @@ namespace Qwack.Models.MCModels
                 if (Model.GetVolSurface(assetId) is not IATMVolSurface surface)
                     throw new Exception($"Vol surface for asset {assetId} could not be cast to IATMVolSurface");
                 var fixingDict = fixingsNeeded.ContainsKey(assetId) ? Model.GetFixingDictionary(assetId) : null;
-                var fixings = fixingDict.ToDictionary(x=>x.Key,x=>x.Value); /*fixingDict != null ?
+                var fixings = fixingDict != null ? fixingDict.ToDictionary(x=>x.Key,x=>x.Value) : new Dictionary<DateTime, double>(); /*fixingDict != null ?
                     fixingsNeeded[assetId].ToDictionary(x => x, x => fixingDict.GetFixing(x))
-                    : new Dictionary<DateTime, double>();*/
+                    */
                 var futuresSim = Settings.ExpensiveFuturesSimulation &&
                    (Model.GetPriceCurve(assetId).CurveType == PriceCurveType.ICE || Model.GetPriceCurve(assetId).CurveType == PriceCurveType.NYMEX);
                 if (futuresSim)

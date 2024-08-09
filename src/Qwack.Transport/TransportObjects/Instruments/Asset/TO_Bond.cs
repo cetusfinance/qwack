806fd09488d7275878cd17754ff7ed95bd8eb07e
Gavin Brennan <gavbrennan@hotmail.com>
2024-08-09 13:32:16 +0100

    TO_Bond serialization fix

--- src/Qwack.Transport/TransportObjects/Instruments/Asset/TO_Bond.cs
+++ src/Qwack.Transport/TransportObjects/Instruments/Asset/TO_Bond.cs
@@ -8,14 +8,21 @@ namespace Qwack.Transport.TransportObjects.Instruments.Asset
     [ProtoContract]
     public class TO_Bond : TO_CashAsset
     {
+        [ProtoMember(21)]
         public double? Coupon { get; set; }
+        [ProtoMember(22)]
         public CouponFrequency CouponFrequency { get; set; }
 
+        [ProtoMember(23)]
         public DateTime? IssueDate { get;set; }
+        [ProtoMember(24)]
         public DateTime? MaturityDate { get; set; }
+        [ProtoMember(25)]
         public DateTime? FirstCouponDate { get; set; }
 
+        [ProtoMember(26)]
         public Dictionary<DateTime, double> CallSchedule { get; set; } 
+        [ProtoMember(27)]
         public Dictionary<DateTime, double> SinkingSchedule { get; set; } 
     }
 }
